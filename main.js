a56335d8445581f2fe5c0daf6e924fedf6516e67
Maxime Druart <44201023+MaximeDruart@users.noreply.github.com>
2021-05-24 18:13:11 +0200

    moved glsl to a separate file and handling canvas logic in js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import "./style.css"
+import fragShader from "./frag.glsl?raw"
 
 import GlslCanvas from "glslCanvas"
 
 const canvas = document.querySelector(".glslCanvas")
-
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
 var sandbox = new GlslCanvas(canvas)
+sandbox.load(fragShader)
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+})
