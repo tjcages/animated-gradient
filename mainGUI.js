199c9e970a2a83c7afc3c4e4764da40e37896c81
Maxime Druart <44201023+MaximeDruart@users.noreply.github.com>
2021-05-27 15:53:36 +0200

    added color picker

--- mainGUI.js
+++ mainGUI.js
@@ -22,6 +22,9 @@ const params = {
   mouseImpact: 0.2,
   bleedOnly: false,
   blackAndWhiteMode: false,
+}
+
+const palette = {
   colorA: [0.486 * 255, 0.69 * 255, 0.996 * 255],
   colorB: [0.909 * 255, 0.258 * 255, 0 * 255],
   colorC: [1 * 255, 0.83 * 255, 0.356 * 255],
@@ -37,9 +40,20 @@ gui.add(params, "noiseStrength", 0.005, 0.1).onChange(updateUniforms)
 gui.add(params, "mouseImpact", 0.05, 0.6).onChange(updateUniforms)
 gui.add(params, "bleedOnly").onChange(updateUniforms)
 gui.add(params, "blackAndWhiteMode").onChange(updateUniforms)
-gui.addColor(params, "colorA").onChange(updateUniforms)
-gui.addColor(params, "colorB").onChange(updateUniforms)
-gui.addColor(params, "colorC").onChange(updateUniforms)
+gui.addColor(palette, "colorA").onChange(updateUniforms)
+gui.addColor(palette, "colorB").onChange(updateUniforms)
+gui.addColor(palette, "colorC").onChange(updateUniforms)
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null
+}
 
 function updateUniforms() {
   sandbox.setUniform("u_speed", params.speed)
@@ -52,10 +66,20 @@ function updateUniforms() {
   sandbox.setUniform("u_mouse_impact", params.mouseImpact)
   // plus sign converts to number
   sandbox.setUniform("u_bleed_only", +params.bleedOnly)
-  if (!params.blackAndWhiteMode) {
-    sandbox.setUniform("u_color_a", params.colorA[0] / 255, params.colorA[1] / 255, params.colorA[2] / 255)
-    sandbox.setUniform("u_color_b", params.colorB[0] / 255, params.colorB[1] / 255, params.colorB[2] / 255)
-    sandbox.setUniform("u_color_c", params.colorC[0] / 255, params.colorC[1] / 255, params.colorC[2] / 255)
+
+  console.log(typeof palette.colorA, hexToRgb(palette.colorA))
+
+  for (const color in palette) {
+    if (typeof palette[color] === "string") {
+      const { r, g, b } = hexToRgb(palette[color])
+      palette[color] = [r, g, b]
+    }
+  }
+
+  if (!palette.blackAndWhiteMode) {
+    sandbox.setUniform("u_color_a", palette.colorA[0] / 255, palette.colorA[1] / 255, palette.colorA[2] / 255)
+    sandbox.setUniform("u_color_b", palette.colorB[0] / 255, palette.colorB[1] / 255, palette.colorB[2] / 255)
+    sandbox.setUniform("u_color_c", palette.colorC[0] / 255, palette.colorC[1] / 255, palette.colorC[2] / 255)
   } else {
     sandbox.setUniform("u_color_a", 0, 0, 0)
     sandbox.setUniform("u_color_b", 0.5, 0.5, 0.5)
